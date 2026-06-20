2d1dc7810d6e4b8ea4f8228560f2e6a056891060
CarolinHugo <hugo.carolin@epitech.eu>
2026-06-20 22:17:00 +0400

    Feat: add favicon Mairie360

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,28 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Mairie360 E-learning",
   description: "Module de formation en ligne Mairie360",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      {
+        url: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
